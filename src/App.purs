db15f48f975e6e9d09f5fc73acca86acc0c2f17e
Kharchev <kharcchevbv@gmail.com>
2023-01-13 22:01:17 +0400

    added confirm add modal

--- src/App.purs
+++ src/App.purs
@@ -37,37 +37,25 @@ import Data.Array (mapWithIndex)
 import HTML.Utils (whenElem)
 import Store as Store
 
--- core component
--- here we have: list of money items, total amount and "move" modal,
---    plus button (to add new mitem), "pop-up notifications" (like portal)
--- it should also probably manage the state: fetch the items and currencies
--- and then set it into store (?)
--- or maybe it just manages the state locally and provides it to children
-
--- lifecycle: on init load currencies and articles
--- state: currencies, mitems, isCreating
--- action: initialize, set currencies, set mitems, add "new" (editing) item,
--- update item, delete item
--- input: no
--- render "loader" if isLoading, render array of mitems; if isCreating, render CreateMoneyItem;
--- render notifications (connected)
-
 data WAction
     = Initialize
     | Receive (Connected (RemoteData String (Array MoneyItemWithId)) Unit)
     | HandleMoneyItemUpdate { item :: MoneyItem.Input, id :: Int }
-    | HandleNewItemOutput CreateMoneyItem.Output
+    | HandleNewItemCancel
     | HandleAddNewButtonOutput Button.Output
     | ShowConfirmDeleteModal Int
     | HandleDeleteModalClose Modal.Output Int
+    | ShowConfirmAddModal CreateMoneyItem.State
+    | HandleAddModalClose Modal.Output CreateMoneyItem.State
 
-data ConfirmDeleteModalState = Hidden | Visible Int -- id of the item to delete on confirm
+data ModalState a = Hidden | Visible a
 
 type State =
     { isCreating :: Boolean
     , isInitialized :: Boolean
     , moneyItems :: RemoteData String (Array MoneyItemWithId)
-    , confirmDeleteModal :: ConfirmDeleteModalState
+    , confirmDeleteModal :: ModalState Int -- id of the item to delete on confirm
+    , confirmAddModal :: ModalState CreateMoneyItem.State -- item data to add on confirm
     }
 
 type Slots =
@@ -100,7 +88,8 @@ app = connect selectMoneyItems $ H.mkComponent
         { isCreating: false
         , isInitialized: false
         , moneyItems
-        , confirmDeleteModal: Hidden }
+        , confirmDeleteModal: Hidden
+        , confirmAddModal: Hidden }
     handleAction :: WAction -> H.HalogenM State WAction Slots o m Unit
     handleAction = case _ of
        Initialize -> do
@@ -126,24 +115,30 @@ app = connect selectMoneyItems $ H.mkComponent
             H.modify_ _ { confirmDeleteModal = Hidden }
        HandleAddNewButtonOutput _ -> do
             H.modify_ _ { isCreating = true }
-       HandleNewItemOutput output -> case output of
-            CreateMoneyItem.Cancelled -> H.modify_ _ { isCreating = false }
-            CreateMoneyItem.Confirmed state -> do
-                result <- createMoneyItem state
-                case result of
-                    Just newItem -> updateStore $ AddMoneyItem newItem
-                    _ -> pure unit -- show error
-                H.modify_ _ { isCreating = false }
+       ShowConfirmAddModal item -> do
+           H.modify_ _ { confirmAddModal = Visible item }
+       HandleAddModalClose closeResult item -> do
+            case closeResult of
+                Modal.Confirmed -> do
+                    result <- createMoneyItem item
+                    case result of
+                        Just newItem -> updateStore $ AddMoneyItem newItem
+                        _ -> pure unit -- show error
+                    H.modify_ _ { isCreating = false }
+                _ -> pure unit
+            H.modify_ _ { confirmAddModal = Hidden }
+       HandleNewItemCancel -> H.modify_ _ { isCreating = false }
        _ -> do
             pure unit
 
     render :: State -> H.ComponentHTML WAction Slots m
-    render { isCreating, isInitialized, moneyItems, confirmDeleteModal } =
+    render { isCreating, isInitialized, moneyItems, confirmDeleteModal, confirmAddModal } =
         HH.div []
             [ renderMoneyItems moneyItems
             , whenElem isCreating newItem
             , whenElem (not isCreating) addNewButton
             , renderConfirmDeleteModal
+            , renderConfirmAddModal
             ]
         where
             renderMoneyItem :: MoneyItemWithId -> _
@@ -156,9 +151,15 @@ app = connect selectMoneyItems $ H.mkComponent
             renderMoneyItems = case _ of
                 Success arr -> HH.div_ $ map renderMoneyItem arr
                 _ -> HH.text "nothing"
-            newItem _ = HH.slot _createItem 0 CreateMoneyItem.createMoneyItem unit HandleNewItemOutput
+            newItem _ = HH.slot _createItem 0 CreateMoneyItem.createMoneyItem unit (\output -> case output of
+                CreateMoneyItem.Confirmed state -> ShowConfirmAddModal state
+                CreateMoneyItem.Cancelled -> HandleNewItemCancel)
             addNewButton _ = HH.slot _button 0 Button.button { text: "Add new" } HandleAddNewButtonOutput
             renderConfirmDeleteModal = case confirmDeleteModal of
                 Hidden -> HH.text ""
-                Visible id -> HH.slot _modal 0 Modal.modal { title: "Confirm", text: "Are you sure?" }
+                Visible id -> HH.slot _modal 0 Modal.modal { title: "Confirm", text: "Are you sure you want to delte this item?" }
                     (\modalResult -> HandleDeleteModalClose modalResult id)
+            renderConfirmAddModal = case confirmAddModal of
+                Hidden -> HH.text ""
+                Visible item -> HH.slot _modal 0 Modal.modal { title: "Confirm", text: "Are you sure you want to add this item?" }
+                    (\modalResult -> HandleAddModalClose modalResult item)
